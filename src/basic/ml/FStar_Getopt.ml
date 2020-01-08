e7920712b01213e3229a88802af5423ea4e3d20f
Tahina Ramananandro <taramana@microsoft.com>
2020-01-08 13:50:33 -0800

    with @nikswamy, work around ocaml-batteries-team/batteries-included#845
    this Batteries change used to break parsing of option arguments
    surrounded by single quotes. this commit fixes it
    
    in fact, we were also partly guilty using physical equality with
    strings...

--- src/basic/ml/FStar_Getopt.ml
+++ src/basic/ml/FStar_Getopt.ml
@@ -52,7 +52,7 @@ let parse_cmdline specs others =
 let parse_string specs others (str:string) =
     let split_spaces (str:string) =
       let seps = [int_of_char ' '; int_of_char '\t'] in
-      FStar_List.filter (fun s -> s != "") (FStar_String.split seps str)
+      FStar_List.filter (fun s -> s <> "") (FStar_String.split seps str)
     in
     (* to match the style of the F# code in FStar.GetOpt.fs *)
     let index_of str c =
