ef70220e7475fe17f559b3c6622db2fdc625277a
Guido Martínez <mtzguido@gmail.com>
2020-03-10 12:07:03 -0300

    normalizer: do not change delta_level when descending into a match

--- src/typechecker/FStar.TypeChecker.Normalize.fs
+++ src/typechecker/FStar.TypeChecker.Normalize.fs
@@ -2405,22 +2405,8 @@ and rebuild (cfg:cfg) (env:env) (stack:stack) (t:term) : term =
           // If either Weak or HNF, then don't descend into branch
           let whnf = cfg.steps.weak || cfg.steps.hnf in
           let cfg_exclude_zeta =
-             let new_delta =
-               cfg.delta_level |> List.filter (function
-                 | Env.InliningDelta
-                 | Env.Eager_unfolding_only -> true
-                 | _ -> false)
-             in
-             let steps = {
-                    cfg.steps with
-                    zeta = false;
-                    unfold_until = None;
-                    unfold_only = None;
-                    unfold_attr = None;
-                    unfold_tac = false
-             }
-             in
-            ({cfg with delta_level=new_delta; steps=steps; strong=true})
+            let steps = { cfg.steps with zeta = false } in
+            {cfg with steps=steps; strong=true}
           in
           let norm_or_whnf env t =
             if whnf
